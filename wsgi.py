de3941a55ccf597b0195a2a6466d43d5773d5bfb
Serhii Horodilov <sgorodil@gmail.com>
2022-09-14 13:16:44 +0300

    Implemented user signup
    Create signup form and setup form validators.
    Updated signup template.
    Updated signup route with actual user creation on success validation.

--- wsgi.py
+++ wsgi.py
@@ -14,11 +14,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import (
     BooleanField,
-    PasswordField,
+    EmailField, PasswordField,
     StringField,
     SubmitField
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
 
 from utils import get_log, logs
 
@@ -59,10 +59,41 @@ class UserModel(db.Model):
         """Check passed raw password with user assigned"""
 
         # TODO: add implementation
-        return True
+
+    @staticmethod
+    def generate_hashed_password(raw_password: str) -> str:
+        """Return hashed password"""
+
+        # TODO: add implementation
+        return raw_password
 
 
 # authentication forms
+class SignUpForm(FlaskForm):
+    """New user form"""
+
+    username = StringField("Username", [DataRequired(), Length(max=128)])
+    password = PasswordField("Password", [DataRequired(), Length(max=128)])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    email = EmailField("Email", [DataRequired()])
+    first_name = StringField("First Name", [Length(max=64)])
+    last_name = StringField("Last Name", [Length(max=64)])
+
+    submit = SubmitField("Sign Up")
+
+    # noinspection PyMethodMayBeStatic
+    def validate_username(self, username: StringField) -> None:
+        user = UserModel.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("This username is already registered.")
+
+    # noinspection PyMethodMayBeStatic
+    def validate_email(self, email: StringField) -> None:
+        user = UserModel.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("This email is already registered.")
 
 
 class SignInForm(FlaskForm):
@@ -76,11 +107,26 @@ class SignInForm(FlaskForm):
 
 
 # authentication routes
-@application.route("/signup/")
+@application.route("/signup/", methods=["GET", "POST"])
 def signup():
     """New user registration route"""
 
-    return render_template("signup.html")
+    form = SignUpForm()
+    if form.validate_on_submit():
+        db.session.add(
+            UserModel(
+                username=form.username.data,
+                email=form.email.data,
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                password=UserModel.generate_hashed_password(form.password.data)
+            )
+        )
+        db.session.commit()
+        flash("Account created. You can login now.", "success")
+        return redirect(url_for("signin"))
+
+    return render_template("signup.html", form=form)
 
 
 @application.route("/signin/", methods=["GET", "POST"])
